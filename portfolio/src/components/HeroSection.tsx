379b602c3f0080c7100ae56559e5a9c8ae5678f9
Rayan Jowair <rayanaljowair@gmail.com>
2023-05-23 08:38:23 +0300

    add illistration to the hero section

--- portfolio/src/components/HeroSection.tsx
+++ portfolio/src/components/HeroSection.tsx
@@ -3,21 +3,31 @@ import React from "react";
 import { motion } from "framer-motion";
 
 import PersonalImage from "../assets/personal-img.svg";
+import PersonalImageOutline from "../assets/personal-img-outline.svg";
+import HeaderImage from "../assets/HeroSection-Image.svg";
 import SVGCircle from "../assets/circle.svg";
 
 function HeroSection() {
   return (
     <header className="personal-photo w-50% flex justify-center columns-2 gap-5 sm:gap-10 pt-12 sm:pt-52">
-      <img
-        className="w-40 sm:w-52 md:w-96"
-        src={PersonalImage}
-        alt="Rayan Photo"
-      />
+      <div className="relative">
+        <motion.img
+          className="w-40 sm:w-52 md:w-96"
+          src={PersonalImage}
+          alt="Rayan Photo"
+        />
+        {/* <img
+          className="absolute w-40 sm:w-52 md:w-96 top-0 right-0 bottom-0 left-0"
+          src={PersonalImageOutline}
+          alt="Rayan Photo"
+        /> */}
+      </div>
+
       <div className="name-roles-container flex flex-col justify-center text-center gap-3 sm:gap-5">
         <h1 className="font-bold text-3xl z-10 sm:text-5xl md:text-8xl sm:py-5">
           Rayan Jowair
         </h1>
-        <h2 className="font-medium text-xl z-10 sm:text-3xl md:text-4xl">
+        <h2 className="font-thin text-xl z-10 sm:text-3xl md:text-4xl">
           Front End Developer
         </h2>
         <motion.img
@@ -32,7 +42,7 @@ function HeroSection() {
           }}
           transition={{ duration: 0.5 }}
         />
-        <h2 className="font-medium text-xl z-10 sm:text-3xl md:text-4xl">
+        <h2 className="font-thin text-xl z-10 sm:text-3xl md:text-4xl">
           UX / UI Designer
         </h2>
       </div>
--- portfolio/src/components/IndexPage.tsx
+++ portfolio/src/components/IndexPage.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
+import { motion } from "framer-motion";
+import HeaderImage from "../assets/HeroSection-Image.svg";
 
 function IndexPage() {
   return (
     <div className="flex flex-col">
       <Navbar />
       <HeroSection />
+      <motion.img
+        className="w-1/2 self-end"
+        src={HeaderImage}
+        alt="Header Image"
+        transition={{ duration: 3 }}
+      />
     </div>
   );
 }
